9afa64fcd73a348b67a3a7e4a8d862e4fc96a23c
Seth Gonzales <sethgonzales157@gmail.com>
2023-08-15 16:50:17 -0700

    create text analyzer

---
+++ js/scripts.js
@@ -0,0 +1,86 @@
+// //Business Logic
+// // function wordCounter(text) {
+// //     if (text.trim().length === 0) {
+// //         return 0;
+// //     }
+
+// //     let wordCount = 0;
+// //     const textArray = text.split(" ");
+// //     textArray.forEach(function (element) {
+// //         if (!Number(element)) {
+// //             wordCount++;
+// //         }
+// //     });
+// //     return wordCount;
+// // }
+
+// function numberOfOccurrencesInText(word, text) {
+//     const textArray = text.split(" ");
+//     let wordCount = 0;
+//     textArray.forEach(function (element) {
+//         if (element.toLowerCase().includes(word.ToLowerCase())) {
+//             wordCount++;
+//         }
+//     });
+//     return wordCount;
+// }
+
+// // UI Logic
+
+// function handleFormSubmission() {
+//     event.preventDefault();
+//     const passage = document.getElementById("text-passage").value;
+//     const word = document.getElementById("word").value;
+//     const wordCount = wordCounter(passage);
+//     const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+//     document.getElementById("total-count").innerText = wordCount;
+//     document.getElementById("selected-count").innerText = occurrencesOfWord;
+//   }
+  
+//   window.addEventListener("load", function() {
+//     document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+//   });
+
+
+// Business Logic
+
+function wordCounter(text) {
+    if (text.trim().length === 0) {
+      return 0;
+    }
+    let wordCount = 0;
+    const textArray = text.split(" ");
+    textArray.forEach(function(element) {
+      if (!Number(element)) {
+        wordCount++;
+      }
+    });
+    return wordCount;
+  }
+  
+  function numberOfOccurrencesInText(word, text) {
+    const textArray = text.split(" ");
+    let wordCount = 0;
+    textArray.forEach(function(element) {
+      if (element.toLowerCase().includes(word.toLowerCase())) {
+        wordCount++;
+      }
+    });
+    return wordCount;
+  }
+  
+  // UI Logic
+  
+  function handleFormSubmission() {
+    event.preventDefault();
+    const passage = document.getElementById("text-passage").value;
+    const word = document.getElementById("word").value;
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    document.getElementById("total-count").innerText = wordCount;
+    document.getElementById("selected-count").innerText = occurrencesOfWord;
+  }
+  
+  window.addEventListener("load", function() {
+    document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+  });
